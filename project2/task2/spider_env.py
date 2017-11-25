d4c1a04ff68f4f7b2873ef6fc720a52e19d8af9d
hkshon <hyounguk.shon@gmail.com>
2017-11-25 20:34:38 +0900

    updated env

--- project2/task2/spider_env.py
+++ project2/task2/spider_env.py
@@ -11,10 +11,10 @@ class spider_environment:
         leg_lb = baby_spider_environment()
         leg_rf = baby_spider_environment()
         leg_lf = baby_spider_environment()
-        bitmask_rb = 0b11000000
-        bitmask_lb = 0b00110000
-        bitmask_rf = 0b00001100
-        bitmask_lf = 0b00000011
+        # bitmask_rb = 0b11000000
+        # bitmask_lb = 0b00110000
+        # bitmask_rf = 0b00001100
+        # bitmask_lf = 0b00000011
         self.n_states = 256
         self.n_actions = 256
         self.reward = np.zeros([self.n_states, self.n_actions])
@@ -23,20 +23,43 @@ class spider_environment:
         self.init_state = 0b00001010
         for s in range(self.n_states):
             for a in range(self.n_actions):
-                self.next_state[s,a] = (leg_rb.next_state[(s&bitmask_rb)>>6,(a&bitmask_rb)>>6]<<6)+(leg_lb.next_state[(s&bitmask_lb)>>4,(a&bitmask_lb)>>4]<<4)+(leg_rf.next_state[(s&bitmask_rf)>>2,(a&bitmask_rf)>>2]<<2)+(leg_lf.next_state[(s&bitmask_lf),(a&bitmask_lf)])
+                # strip the state and action bitstrings into each legs
+                s_rb = (s>>6)%4
+                s_lb = (s>>4)%4
+                s_rf = (s>>2)%4
+                s_lf = (s>>0)%4
+                a_rb = (a>>6)%4
+                a_lb = (a>>4)%4
+                a_rf = (a>>2)%4
+                a_lf = (a>>0)%4
+
+                # evaluate next_state
+                self.next_state[s,a] = leg_rb.next_state[s_rb,a_rb]*64+leg_lb.next_state[s_lb,a_lb]*16+leg_rf.next_state[s_rf,a_rf]*4+leg_lf.next_state[s_lf,a_lf]
                 
-                LTACDASDAAAIT = 0b11111111 - (s|self.next_state[s,a]) # bitwise NOR of s and self.next_state[s,a]; indicates legs that are currently down and still down after an action is taken.
-                total_down = (LTACDASDAAAIT>>6)%2 + (LTACDASDAAAIT>>4)%2 + (LTACDASDAAAIT>>2)%2 + (LTACDASDAAAIT)%2
-                total_force = leg_rb.reward[(s&bitmask_rb)>>6,(a&bitmask_rb)>>6] + leg_lb.reward[(s&bitmask_lb)>>4,(a&bitmask_lb)>>4] + leg_rf.reward[(s&bitmask_rf)>>2,(a&bitmask_rf)>>2] + leg_lf.reward[(s&bitmask_lf),(a&bitmask_lf)]
-                if(total_down==0):
+                # evaluate if a leg is currently down and still down after the action a
+                # value is 1 if true.
+                states_NOR = 0b11111111 - (s|self.next_state[s,a]) # bitwise NOR of state and next_state.
+                rb_down = (states_NOR>>6)%2
+                lb_down = (states_NOR>>4)%2
+                rf_down = (states_NOR>>2)%2
+                lf_down = (states_NOR>>0)%2
+                total_down = rb_down+lb_down+rf_down+lf_down
+                
+                # evaluate total force.
+                total_force = leg_rb.reward[s_rb,a_rb] + leg_lb.reward[s_lb,a_lb] + leg_rf.reward[s_rf,a_rf] + leg_lf.reward[s_lf,a_lf]
+
+                # evaluate reward
+                if(total_down==0): 
                     self.reward[s,a] = 0
-                elif(total_down>=3):
+                elif(total_down>=3): 
                     self.reward[s,a] = total_force/total_down
-                elif(total_down==2):
-                    if((((LTACDASDAAAIT>>4)%2) & ((LTACDASDAAAIT>>2)%2)==1) or (((LTACDASDAAAIT>>6)%2) & ((LTACDASDAAAIT)%2)==1) ):
-                        self.reward[s,a] = total_force/total_down
+                elif(total_down==2 and (rb_down*lf_down==1 or lb_down*rf_down==1)):
+                    self.reward[s,a] = total_force/total_down
+                    # if(s==0b01001010 and a==0b01011100): print(self.reward[s,a])
                 else:
                     self.reward[s,a] = 0.25*(total_force/total_down)
+
+
                  
 
 
