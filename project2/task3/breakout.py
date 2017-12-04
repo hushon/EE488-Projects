841a4ca411a9cf0cee9a354fe6e77ea0253d3bef
hkshon <hyounguk.shon@gmail.com>
2017-12-04 21:30:31 +0900

    initial upload

--- project2/task3/breakout.py
+++ project2/task3/breakout.py
@@ -8,6 +8,18 @@ from breakout_ani import *
 from wait import *
 from time import sleep
 
+## define Q network ##
+lr = 0.00025
+nh = 1000 # adjust to avoid overfitting
+depth = 64 # number of convolution filter
+# parameters
+n_episodes = 200
+max_steps = 200
+epsilon = 0.1 #epsilon-greedy factor
+batch_size = 32 #size of a minibatch
+gamma = 0.99 #discount factor
+C = 100 # target network update frequency
+
 class replayMemory:
     def __init__(self):
         self.current_size = 0
@@ -47,57 +59,73 @@ class minibatch:
 env = breakout_environment(5, 8, 3, 1, 2)
 
 sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
-
-## define Q network ##
-lr = 0.001 
-nh = 100 # adjust to avoid overfitting
+### define Q network ##
+#lr = 0.5 
+#nh = 100 # adjust to avoid overfitting
 ni = env.ny*env.nx*env.nf # size of input vector
 no = env.na # size of output vector
+#depth = 16 # number of convolution filter
 x = tf.placeholder(tf.float32, shape=[None, ni])
 y = tf.placeholder(tf.float32, shape=[None, no])
+# Convolution layer
+x_image = tf.reshape(x, [-1, env.ny, env.nx, env.nf])
+W_conv = tf.Variable(name='W_conv', initial_value=tf.truncated_normal([2, 2, 2, depth], stddev=0.1))
+b_conv = tf.Variable(name='b_conv', initial_value=tf.truncated_normal([depth], stddev=0.1))
+h_conv = tf.nn.conv2d(x_image, W_conv, strides=[1, 1, 1, 1], padding='VALID')
+h_relu = tf.nn.relu(h_conv + b_conv)
+h_relu_flat = tf.reshape(h_relu, [-1, 7*4*depth])
 # Hidden layer
-W_h = tf.Variable(name='W_h', initial_value=tf.truncated_normal(shape=[ni, nh], stddev=0.1))
+W_h = tf.Variable(name='W_h', initial_value=tf.truncated_normal(shape=[7*4*depth, nh], stddev=0.1))
 b_h = tf.Variable(name='b_h', initial_value=tf.truncated_normal(shape=[nh], stddev=0.1))
 # W_h = tf.Variable(tf.truncated_normal(shape=[ni, nh], stddev=0.1))
 # b_h = tf.Variable(tf.truncated_normal(shape=[nh], stddev=0.1))
 # W_h = tf.Variable(tf.random_uniform(shape=[ni, nh], minval=0.0, maxval=0.5))
 # b_h = tf.Variable(tf.random_uniform(shape=[nh], minval=0.0, maxval=0.5))
-h = tf.nn.relu(tf.matmul(x, W_h) + b_h)
+h = tf.nn.relu(tf.matmul(h_relu_flat, W_h) + b_h)
 # Output layer
 W_o = tf.Variable(name='W_o', initial_value=tf.truncated_normal(shape=[nh, no], stddev=0.1))
-b_o = tf.Variable(name='b_o', initial_value=tf.truncated_normal(shape=[no], stddev=0.1))
+#W_o = tf.Variable(name='W_o', initial_value=tf.ones(shape=[nh, no]))
+b_o = tf.Variable(name='b_o', initial_value=tf.ones(shape=[no]))
 # W_o = tf.Variable(tf.random_uniform(shape=[nh, no], minval=0.0, maxval=0.5))
 # b_o = tf.Variable(tf.random_uniform(shape=[no], minval=0.0, maxval=0.5))
-Q=tf.matmul(h, W_o) + b_o
+Q=tf.matmul(h, W_o)
+#Q=tf.matmul(h, W_o) + b_o
 # cost function and optimizer
-cost = tf.reduce_mean((y-Q)**2)
+cost = tf.reduce_mean(tf.square(y-Q))
 train_Q = tf.train.AdamOptimizer(lr).minimize(cost)
 sess.run(tf.initialize_all_variables()) # initialize Q and Q_hat
 
 ## define Q_hat network ##
 x_hat = tf.placeholder(tf.float32, shape=[None, ni])
+x_image_hat = tf.reshape(x_hat, [-1,env.ny, env.nx, env.nf])
+W_conv_hat = sess.run(W_conv)
+b_conv_hat = sess.run(b_conv)
+h_conv_hat = tf.nn.conv2d(x_image_hat, W_conv_hat, strides=[1, 1, 1, 1], padding='VALID')
+h_relu_hat = tf.nn.relu(h_conv_hat + b_conv_hat)
+h_relu_flat_hat = tf.reshape(h_relu_hat, [-1, 7*4*depth])
 # W_h_hat = tf.constant(sess.run(W_h))
 # b_h_hat = tf.constant(sess.run(b_h))
 W_h_hat = sess.run(W_h)
 b_h_hat = sess.run(b_h)
-h_hat = tf.nn.relu(tf.matmul(x_hat, W_h_hat) + b_h_hat)
+h_hat = tf.nn.relu(tf.matmul(h_relu_flat_hat, W_h_hat) + b_h_hat)
 # W_o_hat = tf.constant(sess.run(W_o))
 # b_o_hat = tf.constant(sess.run(b_o))
 W_o_hat = sess.run(W_o)
 b_o_hat = sess.run(b_o)
-Q_hat=tf.matmul(h_hat, W_o_hat) + b_o_hat
+Q_hat=tf.matmul(h_hat, W_o_hat)
+#Q_hat=tf.matmul(h_hat, W_o_hat) + b_o_hat
 
-# parameters
-n_episodes = 100
-max_steps = 100
-epsilon = 0.3 #epsilon-greedy factor
-batch_size = 32 #size of a minibatch
-gamma = 0.99 #discount factor
-C = 5 # target network update frequency
+## parameters
+#n_episodes = 100
+#max_steps = 100
+#epsilon = 0.3 #epsilon-greedy factor
+#batch_size = 32 #size of a minibatch
+#gamma = 0.99 #discount factor
+#C = 30 # target network update frequency
 
 batch = minibatch()
 memory = replayMemory() # initialize replay memory
+step=0
 # sess.run(tf.initialize_all_variables()) # initialize Q and Q_hat
 for episode in range(n_episodes):
     s = env.reset() 
@@ -105,11 +133,14 @@ for episode in range(n_episodes):
     print("===episode #", episode, "===")
     for t in range(max_steps): 
         if (np.random.rand() < epsilon): #with probability epsilon select a random action a_t
-            a = np.random.randint(env.na)-1
+            a = np.random.randint(env.na)
         else: #otherwise select a_t = argmax(a, Q(phi(s_t), a; theta))
             q = Q.eval(feed_dict={x: np.reshape(s, [1, env.ny*env.nx*env.nf])})
+#            print("action-value",q)
             a = np.argmax(q)
+            
 
+#        print("action",a)
         sn, r, terminal, _, _, _, _, _, _, _, _ = env.run(a - 1) # action a_t in emulator and observe reward r_t and image x_(t+1)
         memory.store(s, a, r, sn, terminal)
         
@@ -117,23 +148,31 @@ for episode in range(n_episodes):
         y_target = []
         for i in range(batch.size):
             current_Q = Q.eval(feed_dict={x: np.reshape(batch.s[i], [1, env.ny*env.nx*env.nf])})[0]
+#            print("current_Q",current_Q)
             if(batch.terminal[i]==1):
                 a_i = batch.a[i]
                 y_i = batch.r[i]
             else:
                 q_i = Q_hat.eval(feed_dict={x_hat: np.reshape(batch.ns[i], [1, env.ny*env.nx*env.nf])})[0]
-                a_i = np.argmax(q_i)
+#                a_i = np.argmax(q_i)
+                a_i = batch.a[i]
                 y_i = batch.r[i] + gamma*np.max(q_i)
             current_Q[a_i] = y_i
+#            print("a_i", a_i, "y_i", y_i)
+#            print("current_Q modified",current_Q)
             y_target.append(current_Q)
+        
         train_Q.run(feed_dict={x: np.reshape(batch.s, [batch.size, env.ny*env.nx*env.nf]), y: np.array(y_target)})
-
-        if(t%C==0): # every C steps set Q_hat to Q
+        
+        step+=1
+        if(step%C==0): # every C steps set Q_hat to Q
+            print("update target network", step)
+            W_conv_hat = sess.run(W_conv)
+            b_conv_hat = sess.run(b_conv)
             W_h_hat = sess.run(W_h)
             b_h_hat = sess.run(b_h)
             W_o_hat = sess.run(W_o)
             b_o_hat = sess.run(b_o)
-        
 
         s=np.copy(sn)
         episode_reward+=r
@@ -144,10 +183,9 @@ for episode in range(n_episodes):
 
 ## Tensorflow Saver
 saver = tf.train.Saver()
-# saver.restore(sess, "./breakout.ckpt")
 save_path = saver.save(sess, "./breakout.ckpt")
 
-ani = breakout_animation(env, 20)
-# ani.save('breakout.mp4', dpi=200)
+ani = breakout_animation(env, 200, nh, depth)
+ani.save('breakout.mp4', dpi=200)
 plt.show(block=False)
-wait('Press enter to quit')
+# wait('Press enter to quit')
