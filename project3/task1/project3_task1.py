2ea31ae4fb171cef68bac1297c8db6e6a7244726
hkshon <hyounguk.shon@gmail.com>
2017-12-17 11:36:04 +0900

    changed code

--- project3/task1/project3_task1.py
+++ project3/task1/project3_task1.py
@@ -193,6 +193,21 @@ with tf.Session() as sess:
     print(" net plays white: win=%6.4f, loss=%6.4f, tie=%6.4f" %\
         (win2[generation], lose2[generation], tie2[generation]))
 
+    print("Evaluating neural network against random policy")
+    r1 = np.zeros((n_test)) # randomness for player 1
+    r2 = np.ones((n_test))  # randomness for player 2
+    s = game.play_games(P, r1, [], r2, n_test, nargout = 1)
+    win1.append(s[0][0]); lose1.append(s[0][1]); tie1.append(s[0][2]);
+    print(" net plays black: win=%6.4f, loss=%6.4f, tie=%6.4f" %\
+        (win1[generation], lose1[generation], tie1[generation]))
+        
+    r1 = np.ones((n_test))  # randomness for player 1
+    r2 = np.zeros((n_test)) # randomness for player 2
+    s = game.play_games([], r1, P, r2, n_test, nargout = 1)
+    win2.append(s[0][1]); lose2.append(s[0][0]); tie2.append(s[0][2]);
+    print(" net plays white: win=%6.4f, loss=%6.4f, tie=%6.4f" %\
+        (win2[generation], lose2[generation], tie2[generation]))
+
     # saver.save(sess, "./project3_task1.ckpt")
 
 
